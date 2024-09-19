0d1ebc72aea156b6c51cc3ade97d34fa7ecad0e2
gweowe <gweowe@gmail.com>
2024-09-19 16:09:09 +0900

    step3 update

--- main.tf
+++ main.tf
@@ -15,6 +15,6 @@ EOF
 
 resource "terraform_data" "private_provider" {
   provisioner "local-exec" {
-    command = "curl --header \"Authorization\": Bearer ${var.tfe_token}\" --header \"Content-Type: application/vnd.api+json\" --request POST --data @${local.provider_json} https://app.terraform.io/api/v2/organizations/${var.organization_name}/registry-providers"
+    command = "curl --header \"Authorization\": Bearer ${var.tfe_token}\" --header \"Content-Type: application/vnd.api+json\" --request POST --data ${local.provider_json} https://app.terraform.io/api/v2/organizations/${var.organization_name}/registry-providers"
   }
 }
